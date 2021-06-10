88536e8cd801cb5c106b9167dc0ba51465fdd9a0
Denis Makula <emdeevy@gmail.com>
2021-06-10 04:28:30 +0000

    Implemented scripts filename getter;

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+
 class RunnerRetriever {
-    constructor(_message) {
-        console.log(_message);
+
+    constructor(_config) {
+
+        if(!_config || !this.isObject(_config) || _config.directories === undefined || _config.directories.input === undefined || _config.directories.output === undefined || _config.directories.scripts === undefined) {
+            throw new Error("Configuration file is invalid.");
+        }
+
+        this.Directories = {};
+
+        this.Directories.output = _config.directories.output;
+        this.Directories.input = _config.directories.input;
+        this.Directories.scripts = _config.directories.scripts;
+
     }
+
+    getScriptList = () => {
+
+        let scriptsPath = path.join(__dirname, '../../', this.Directories.scripts);
+
+        let output = [];
+
+        fs.readdir(scriptsPath, (_error, _files) => {
+            if(_error) {
+                throw new Error("Unable to scan scripts directory: " + _error);
+            }
+
+            _files.forEach((_file) => {
+                output.push(_file);
+            });
+        });
+
+        return output;
+
+    }
+
+    isObject(_variable) {
+        return (!!_variable) && (_variable.constructor === Object);
+    }
+
 }
 
 module.exports = RunnerRetriever;
